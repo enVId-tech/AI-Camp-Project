255d1e3987ed83b4979896af7c149381b7a81d74
Erick <thelittlebotengineer@gmail.com>
2023-07-21 10:47:14 -0700

    asdfasdfasdfasdf

--- server/python.py
+++ server/python.py
@@ -2,45 +2,83 @@ import os
 import io
 from flask import Flask, render_template, request, send_from_directory, jsonify
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms
+from torchvision.models import resnet50
+from torchvision import models
 from PIL import Image
 from flask_cors import CORS
 import requests
 import uuid
 import base64
 from werkzeug.utils import secure_filename
+import gradio as gr
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
 
 # Configuration
-app.config["IMAGE_UPLOADS"] = "uploads"  # Directory where uploaded files will be stored
-app.config["ALLOWED_EXTENSIONS"] = {"png", "jpg", "jpeg", "gif"}  # Allowed file extensions
+# Directory where uploaded files will be stored
+app.config["IMAGE_UPLOADS"] = "uploads"
+app.config["ALLOWED_EXTENSIONS"] = {
+    "png", "jpg", "jpeg", "gif"}  # Allowed file extensions
 
 # Model Configuration
 MODEL_PATH = "server/model/resnet50.pth"
 
 # Define the model class (you should replace this with your actual model class)
-class MyDataset(torch.nn.Module):
-    def __init__(self):
-        super(MyDataset, self).__init__()
-        # Add the layers of your model here
 
-    def forward(self, x):
-        # Implement the forward pass of your model here
-        return x
 
-# Load the PyTorch model as an OrderedDict
-model_state_dict = torch.load(MODEL_PATH, map_location=torch.device('cpu'))
-print(model_state_dict.keys())
+# Define the model class using ResNet50 as the base model
+# class MyDataset(nn.Module):
+#     def __init__(self):
+#         super(MyDataset, self).__init__()
+#         # Create a new instance of the ResNet50 model
+#         self.resnet = resnet50(pretrained=False)  # Set pretrained=False to load only the architecture
+# 
+#         # Remove the last fully connected layer of ResNet50
+#         self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
+# 
+#     def forward(self, x):
+#         # Perform forward pass through ResNet50
+#         return self.resnet(x)
 
-# Create an instance of your model and load the state_dict
-model = MyDataset()
-model.load_state_dict(model_state_dict)
+# Create an instance of your model
+model = models.resnet50(pretrained=False)
+model.fc = nn.Linear(model.fc.in_features, 5)
 
-# Set the model in evaluation mode
+model.load_state_dict(torch.load('model\\resnet50.bin', map_location=torch.device('cpu')))
+
 model.eval()
+
+image_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+def load_function(image_path):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    image = Image.open(image_path)
+
+    input_img = image_transform(image)
+    input_img_data = input_img.unsqueeze(0)
+    input_batch = input_img_data.to(device)
+
+    model.to(device)
+    model.eval()
+
+    with torch.no_grad():
+        output = model(input_batch)
+
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+    predicted_class = torch.max(probabilities, dim=0)
+
+    print("Predicted class:", predicted_class.indices.item())
+
 
 # Set up the image transformation
 image_transform = transforms.Compose([
@@ -48,18 +86,24 @@ image_transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
+
 def allowed_file(filename):
     """Check if the file has an allowed extension."""
     return "." in filename and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+
 
 def generate_unique_filename(filename):
     """Generate a unique filename to avoid overwriting existing files."""
     unique_id = str(uuid.uuid4())[:8]
     return f"{secure_filename(filename)}_{unique_id}"
 
+
 def query_language_identification(image_data):
     """Send the image data to the language identification model and get the response."""
     try:
+
+        image_labels = {0: "Arabic", 1: "Chinese", 2: "Gujarati", 3: "Tamil", 4: "Thai"}
+
         # Load image from image data
         img = Image.open(io.BytesIO(image_data))
         img = image_transform(img).unsqueeze(0)  # Add batch dimension
@@ -72,14 +116,16 @@ def query_language_identification(image_data):
         probabilities = F.softmax(output, dim=1).squeeze(0)
         _, predicted_class = torch.max(probabilities, dim=0)
 
-        return {"predicted_class": predicted_class.item(), "probabilities": probabilities.tolist()}
+        return {"predicted_class": image_labels[predicted_class.item()], "probabilities": probabilities.tolist()}
 
     except Exception as e:
         return {"error": str(e)}
 
+
 @app.route('/')
 def main():
     return render_template("index.html")
+
 
 @app.route('/predict', methods=['POST'])
 def success():
@@ -109,12 +155,33 @@ def success():
 
         return jsonify(output)
 
+
 @app.route('/uploads/<filename>')
 def serve_uploaded_file(filename):
     """Safely serve the uploaded file from the 'uploads' directory."""
     return send_from_directory(app.config["IMAGE_UPLOADS"], filename)
 
+
 if __name__ == "__main__":
     # Ensure the 'uploads' directory exists
     os.makedirs(app.config["IMAGE_UPLOADS"], exist_ok=True)
     app.run(host='0.0.0.0', port=5000)
+
+
+def predict(img):
+    img = image_transform(img).unsqueeze(0)  # Add batch dimension
+
+    model.eval()
+    with torch.no_grad():
+        output = model(img)
+
+    probabilities = F.softmax(output, dim=1).squeeze(0)
+    _, predicted_class = torch.max(probabilities, dim=0)
+
+    return {"predicted_class": predicted_class.item(), "probabilities": probabilities.tolist()}
+
+
+inputs = gr.inputs.Image()
+outputs = gr.outputs.Label(num_top_classes=3)
+
+gr.Interface(fn=predict, inputs=inputs, outputs=outputs).launch()
